3df8785ca230f05891b86bb24119faad1f30f0a6
Whoplaros <whoplaros@gmail.com>
2024-10-22 20:59:24 +0300

    tinkering with moment.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+let now = new Date();
+console.log(now);
+let msec = Date.parse(now);
+console.log(msec);
+let birthday = new Date("October 16, 1996 05:35:32");
+
+console.log(msec);
+
+let today = moment().format("dddd Do MMMM");
+console.log(today);
