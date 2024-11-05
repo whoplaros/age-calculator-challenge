04264013112bede79001e5e7641f2abb40e04dbb
Whoplaros <whoplaros@gmail.com>
2024-11-05 15:57:42 +0200

    adding some js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-console.log("Hello");
-let now = new Date();
-console.log(now);
-let msec = Date.parse(now);
-console.log(msec);
-let birthday = new Date("October 16, 1996 05:35:32");
+// Function to calculate age
+function calculateAge() {
+	var day1 = document.getElementById("birthday-day").value;
+	var month1 = document.getElementById("birthday-month").value;
+	var year1 = document.getElementById("birthday-year").value;
 
-console.log(msec);
+	var date = new Date();
+	var day2 = date.getDate();
+	var month2 = 1 + date.getMonth();
+	var year2 = date.getFullYear();
+	var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let today = moment().format("dddd Do MMMM");
-console.log(today);
+	if (day1 > day2) {
+		day2 = day2 + month[month2 - 1];
+		month2 = month2 - 1;
+	}
+	if (month1 > month2) {
+		month2 = month2 + 12;
+		year2 = year2 - 1;
+	}
+	var day = day2 - day1;
+	var month = month2 - month1;
+	var year = year2 - year1;
+
+	console.log(day, month, year);
+}
